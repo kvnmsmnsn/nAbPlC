5b8f87a4850a89ba2a9c54a2af69570ffae72f61
Kevin Simonson <10956290@uvu.edu>
2026-07-04 18:26:16 +0000

    First attempt Lab 6.1.

--- mag/tcl/lvs_netgen.tcl
+++ mag/tcl/lvs_netgen.tcl
@@ -9,6 +9,6 @@ readnet verilog ../src/project.v $source
 # readnet verilog ../verilog/gl/your_design.v $source
 
 # add any spice files of your analog blocks:
-readnet spice ../xschem/simulation/$project.spice $source
+readnet spice ../xschem/simulation/nAbPlC.spice $source
 
 lvs "$layout $project" "$source $project" $::env(PDK_ROOT)/sky130A/libs.tech/netgen/sky130A_setup.tcl lvs.report -blackbox
